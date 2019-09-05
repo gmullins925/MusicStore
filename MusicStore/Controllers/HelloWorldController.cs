873f2f06fe4df0585d8c3a0992a96511eed4dc74
Gary Mullins <gmullins925@outlook.com>
2019-09-05 16:52:46 -0500

    MVC Lab1 Commit

---
+++ MusicStore/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Neo...the Matrix has you...";
+		}
+    }
+}
