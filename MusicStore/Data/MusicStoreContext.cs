4fc90091e13351dd421651959d8f3d4a70042fa7
Gary Mullins <gmullins925@outlook.com>
2019-09-05 17:52:47 -0500

    Add Controller Music

---
+++ MusicStore/Data/MusicStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicStore.Models
+{
+    public class MusicStoreContext : DbContext
+    {
+        public MusicStoreContext (DbContextOptions<MusicStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MusicStore.Models.Music> Music { get; set; }
+    }
+}
